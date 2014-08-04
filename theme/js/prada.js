045554f2c4fe3dee13f68e190af547bacfac3dcd
jaygidwitz <jay.gidwitz@gmail.com>
2014-08-04 17:37:13 -0400

    ridiculous amount of changes, still working on
    responsive design

--- theme/js/prada.js
+++ theme/js/prada.js
@@ -1,43 +1,3 @@
-/*
-
-(function Fooooooooter($){
-// Window load event used just in case window height is dependant upon images
-$(window).bind("load", function() { 
-       
-       var footerHeight = 0,
-           footerTop = 0,
-           $footer = $(".site-footer");
-           
-       positionFooter();
-       
-       function positionFooter() {
-       
-                footerHeight = $footer.height();
-                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
-       
-               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
-                   $footer.css({
-                        position: "absolute"
-                   }).animate({
-                        top: footerTop
-                   })
-               } else {
-                   $footer.css({
-                        position: "static"
-                   })
-               }
-               
-       }
-
-       $(window)
-               .scroll(positionFooter)
-               .resize(positionFooter)
-               
-});
-
-})(jQuery);
-*/
-/*
 (function stickyFooter($){
 
 	var stickyClass = 'sticky-bottom';
@@ -72,7 +32,7 @@ $(window).bind("load", function() {
 		// On Resize
 		$(window).resize(stickToBottom);
 	}
-})(jQuery);*/
+})(jQuery);
 
 (function animatedBgHomepage($){
 	
